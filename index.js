62568cab6b35ac817fde29eb4fc19f5049b73d96
fernando stefoni <fer.st1995@gmail.com>
2023-09-19 08:42:36 -0300

    Practice 1

---
+++ index.js
@@ -0,0 +1,104 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+
+    generateProductId() {
+        const productId = (this.products.length + 1).toString();
+        return productId;
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const isCodeUnique = this.products.every((product) => product.code !== code);
+
+        if (!isCodeUnique) {
+            throw new Error("El código del producto ya está en uso.");
+        }
+
+        const productId = this.generateProductId();
+        const newProduct = {
+            id: productId,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        };
+
+        this.products.push(newProduct);
+        return newProduct;
+    }
+
+    getProductById(id) {
+        const product = this.products.find((product) => product.id === id);
+
+        if (!product) {
+            console.error("Not found");
+            return null;
+        }
+
+        return product;
+    }
+}
+
+const productManager = new ProductManager();
+
+
+console.log(productManager.getProducts());
+
+
+const newProduct = productManager.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+);
+productManager.addProduct(
+    "Producto 2",
+    "Descripción del producto 2",
+    100,
+    "Imagen 2",
+    "code1",
+    10
+);
+
+productManager.addProduct(
+    "Producto 3",
+    "Descripción del producto 3",
+    150,
+    "Imagen 3",
+    "code2",
+    20
+);
+console.log("Producto agregado:", newProduct);
+console.log(productManager.getProducts());
+
+try {
+    productManager.addProduct(
+        "producto prueba",
+        "Este es un producto prueba",
+        200,
+        "Sin imagen",
+        "abc123",
+        25
+    );
+} catch (error) {
+    console.error("Error al agregar producto duplicado:", error.message);
+}
+
+
+const productIdToFind = '2'; 
+const foundProduct = productManager.getProductById(productIdToFind);
+
+if (foundProduct !== null) {
+    console.log("Producto encontrado por ID:", foundProduct);
+} else {
+    console.error("Producto no encontrado");
+}
